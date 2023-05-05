de95e576d112046a816eb107250d25243400d4ab
EvgenyMuseev <112461652+EvgenyMuseev@users.noreply.github.com>
2023-05-05 14:47:41 +0300

    Initial commit

---
+++ src/components/SlateEditor/Toolbar/toolbarGroups.js
@@ -0,0 +1,45 @@
+const toolbarGroups = [
+  [
+    {
+      id: 3,
+      format: "bold",
+      type: "mark"
+    },
+    {
+      id: 4,
+      format: "italic",
+      type: "mark"
+    },
+    {
+      id: 5,
+      format: "underline",
+      type: "mark"
+    },
+    {
+      id: 6,
+      format: "strikethrough",
+      type: "mark"
+    }
+  ],
+  [
+    {
+      id: 15,
+      format: "orderedList",
+      type: "block"
+    },
+    {
+      id: 16,
+      format: "unorderedList",
+      type: "block"
+    }
+  ],
+  [
+    {
+      id: 20,
+      format: "link",
+      type: "link"
+    }
+  ]
+];
+
+export default toolbarGroups;
